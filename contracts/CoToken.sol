29da89c7c0608710ab9a9c0690531a73651455fd
Kaluba Chikonde <kaluba.c96@gmail.com>
2019-08-03 21:26:55 +0200

    update CoToken

--- contracts/CoToken.sol
+++ contracts/CoToken.sol
@@ -10,18 +10,18 @@ contract CoToken is Ownable,ERC20 {
      uint256 constant public decimals = 10**18;
       uint public _totalSupply;
 
-    function buyPrice(uint x) public returns(uint) {
+    function buyPrice(uint x) public pure returns(uint) {
         //1 Ether is 1^18 = 1000000000000000000 Wei.
        uint price = (10**16)*(x)+(2*10**17);
        // uint256 price = ((1/10)*decimals)*(x) + (2/10)*decimals ;
-       return buyprice;
+       return price;
     }
 
-    function sellPrice(uint n) public returns(uint) {
+    function sellPrice(uint n) public pure returns(uint) {
         //1 Ether is 1^18 = 1000000000000000000 Wei.
-      uint sellPrice = (10**16)*(n)+(2*10**17);
+      uint SellPrice = (10**16)*(n)+(2*10**17);
        // uint256 price = ((1/10)*decimals)*(x) + (2/10)*decimals ;
-       return sellPrice;
+       return SellPrice;
     }
 
     function mint(uint amount) public payable {
@@ -32,12 +32,15 @@ contract CoToken is Ownable,ERC20 {
 
     }
 
-    function burn(uint256 value) onlyOwner public {
+    function burn(uint256 value) onlyOwner public payable {
          require(msg.value >= sellPrice(value));
 
         _totalSupply = _totalSupply - value;
         // _balances[account] = _balances[account].sub(value); // should burn from all accounts
     }
 
+    function destroy() public onlyOwner { //onlyOwner is custom modifier
+        selfdestruct(msg.sender);
 
+    }
 }
