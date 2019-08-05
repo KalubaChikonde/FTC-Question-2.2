18b61907c834ff17eeb87d6a29761ce365bc2852
Kaluba Chikonde <kaluba.c96@gmail.com>
2019-08-05 07:46:54 +0200

    updated testCoToken.js

--- contracts/CoToken.sol
+++ contracts/CoToken.sol
@@ -8,41 +8,61 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 //import "installed_contracts/zeppelin/contracts/math/Ownable.sol";
 
+
+
 contract CoToken is Ownable,ERC20 {
    
       uint public totalSupply_ = 0;
       uint public buyPrice_;
       uint public sellPrice_;
+    
+      
+    
 
-    function buyPrice() public returns(uint) {
+    function buyPrice(uint256 _tokens) public returns(uint) {
         //1 Ether is 1^18 = 1000000000000000000 Wei.
-       buyPrice_ = (10**16)*(totalSupply_)+(2*10**17);
-       // uint256 price = ((1/10)*decimals)*(x) + (2/10)*decimals ;
+       uint upperBound = totalSupply_ + _tokens;
+      // buyPrice_  = ((5*(10**15))*(_tokens)*(_tokens)) + ((2*10**17)*(_tokens)); 
+       //buyPrice_ = ((upperBound * 10**16)**2) + (upperBound  * 2*10**17) - poolBalance();
+           buyPrice_ = ((5*(10**15))*(upperBound)*(upperBound)) + ((2*10**17)*(upperBound)) - poolBalance();
+      
        return buyPrice_;
     }
 
-    function sellPrice() public returns(uint) {
+      //total ether held by bonding curve
+    function poolBalance() public view returns(uint) {
+      
+      uint poolBalance_ = ((5*(10**15))*(totalSupply_)*(totalSupply_)) + ((2*10**17)*(totalSupply_));
+        return poolBalance_;
+    }
+
+    function sellPrice(uint256 _tokens) public returns(uint) {
         //1 Ether is 1^18 = 1000000000000000000 Wei.
-        sellPrice_ = (10**16)*(totalSupply_)+(2*10**17);
-       // uint256 price = ((1/10)*decimals)*(x) + (2/10)*decimals ;
+        uint upperBound = totalSupply_ - _tokens;
+               sellPrice_ =  poolBalance() -  ((5*(10**15))*(upperBound)*(upperBound)) - ((2*10**17)*(upperBound)) ;
        return sellPrice_;
     }
 
-    function mint(uint amount) public payable {
-        require(msg.value >= buyPrice_);
-        _mint(msg.sender,amount);
-         totalSupply_ = totalSupply_ + amount;
-
+    //mint tokens 
+    function mint(uint _amount) public payable {
+        require(msg.value >= buyPrice(_amount), "You have insufficient funds");  //checks if there are sufficient funds
+        _mint(msg.sender,_amount); //mint tokens
+         totalSupply_ = totalSupply_ + _amount; //update total supply
+          //poolBalance();
 
     }
-
-    function burn(uint amount) onlyOwner public payable {
-         require(msg.value >= sellPrice_);
-         _burn(msg.sender,amount);
-          totalSupply_ =  totalSupply_ - amount;
-        // _balances[account] = _balances[account].sub(value); // should burn from all accounts
+      
+      //function to burn tokens 
+    function burn(uint _amount) onlyOwner public payable {
+        //CHECK that there are enough funds to burn
+         require(poolBalance() >= sellPrice(_amount),"You have insufficient funds");
+         _burn(msg.sender,_amount);
+          totalSupply_ =  totalSupply_ - _amount; //decrease total supply
+          //poolBalance();
+        
     }
-
+     
+     //destroys contract 
     function destroy() public onlyOwner { //onlyOwner is custom modifier
         selfdestruct(msg.sender);
 
--- test/testCoToken.js
+++ test/testCoToken.js
@@ -3,36 +3,41 @@ const CoToken = artifacts.require('./CoToken.sol')
 
 contract("CoToken",(accounts) => {
     const tokenOwner = accounts[0]
-    const testAddress = accounts[1]
+    const tokenBuyer= accounts[1]
   
     //Initialise total supply, sell price and buy price
     const totalSupply = 0;
+    const mintTokens = 3;
+    const burnTokens = 2;
     const sellPrice = web3.utils.toWei('1', 'ether');
-    const buyPrice = web3.utils.toWei('1', 'ether');
+    const buyPrice = web3.utils.toWei('20', 'ether');
 
-    it("it should mint 10 tokens", async function () {
+    it("it should correctly mint tokens", async function () {
         //fetch instance of contract
         let CoTokenInstance = await CoToken.deployed()
         //call mint function to mint 10 tokens
-        await CoTokenInstance.mint(10, { from: tokenOwner})
+        await CoTokenInstance.mint(mintTokens, { from: tokenOwner, value: buyPrice})
         //retrieve updated balance for token owner
         let balance = await CoTokenInstance.balanceOf(tokenOwner)
-        assert.equal(balance,10,"10 wasn't in tokenOwner")
+        assert.equal(balance,mintTokens,"incorrect number of tokens")
     })
 
-    it("it should burn 4 tokens", async function () {
+    it("it should burn correctly burn tokens", async function () {
         //fetch instance of contract
         let CoTokenInstance = await CoToken.deployed()
         //call burn function to burn 5 tokens
-        await CoTokenInstance.burn(4, { from: tokenOwner})
+        await CoTokenInstance.burn(burnTokens, { from: tokenOwner})
         //retrieve updated balance for token owner
         let balance = await CoTokenInstance.balanceOf(tokenOwner)
-        assert.equal(balance,6,"6 wasn't in tokenOwner")
+        //get number of remaining tokens
+        let tokenBalance = mintTokens - burnTokens
+        assert.equal(balance,tokenBalance,"incorrect number of burn tokens")
     })
 
     it("it should destroy the contract", async function () {
         //fetch instance of contract
         let CoTokenInstance = await CoToken.deployed()
+        
         await CoTokenInstance.destroy( {from: tokenOwner})
         console.log("The contract has been destroyed")
 
